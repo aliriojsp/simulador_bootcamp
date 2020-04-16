bac636db29e6bf2c4420d152528fcf8f7cd37bfc
aliriojsp <32307581+aliriojsp@users.noreply.github.com>
2020-04-16 18:14:19 -0300

    Update server.R

--- server.R
+++ server.R
@@ -4,17 +4,17 @@ library(dplyr)
 
 shinyServer(function(input, output, session) { 
   
- mydata <- reactive({
+ File <- reactive({
 
  urlfile<-'https://raw.githubusercontent.com/aliriojsp/simulador_bootcamp/master/File.csv'
 
 if (is.null(urlfile))
   return(NULL)
 
-File<-read.csv(urlfile)%>%select(Género=Gender,Educación=EducationHighest,ExperienciaPrevia=WorkExperience, Personalidad_apertura="ACPersonalityO", Personalidad_responsabilidad="ACPersonalityC",
+mydata<-read.csv(urlfile)%>%select(Género=Gender,Educación=EducationHighest,ExperienciaPrevia=WorkExperience, Personalidad_apertura="ACPersonalityO", Personalidad_responsabilidad="ACPersonalityC",
 Personalidad_extroversión="ACPersonalityE", Personalidad_amabilidad= "ACPersonalityA", Personalidad_neuroticismo="ACPersonalityN",Competencia_Técnica="ACRatingINTCOMPA", Competencia_Teamplayer="ACRatingINTCOMPB",Competencia_PensamientoCritico="ACRatingINTCOMPC",Competencia_Negocio="ACRatingINTCOMPD",Competencia_InnovacionYmotivacion="ACRatingINTCOMPE", Desempeño_Primeraño="Year1performanceRating")
 
-return(File)
+return(mydata)
 })
 
   
