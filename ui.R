e98b998717a83b66a4ca5f1bd3ef009f43e3ef0e
aliriojsp <32307581+aliriojsp@users.noreply.github.com>
2020-04-16 15:06:55 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,6 +2,10 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
+urlfile<-'https://raw.githubusercontent.com/aliriojsp/simulador_bootcamp/master/File.csv?token=AHWPS7MLS3FOT5NXZOQD7226TCMQA'
+File<-read.csv(urlfile)%>%select(Género=Gender,Educación=EducationHighest,ExperienciaPrevia=WorkExperience, Personalidad_apertura="ACPersonalityO", Personalidad_responsabilidad="ACPersonalityC",
+Personalidad_extroversión="ACPersonalityE", Personalidad_amabilidad= "ACPersonalityA", Personalidad_neuroticismo="ACPersonalityN",Competencia_Técnica="ACRatingINTCOMPA", Competencia_Teamplayer="ACRatingINTCOMPB",Competencia_PensamientoCritico="ACRatingINTCOMPC",Competencia_Negocio="ACRatingINTCOMPD",Competencia_InnovacionYmotivacion="ACRatingINTCOMPE", Desempeño_Primeraño="Year1performanceRating")
+  
 shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
