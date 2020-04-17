db71c9782babd58d40f197738d002254332e8f68
aliriojsp <32307581+aliriojsp@users.noreply.github.com>
2020-04-17 15:50:28 -0300

    Update server.R

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ library(dplyr)
     # Subset the gapminder dataset by the chosen continents
     RemoveGrid<- theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))
     
-    p <- ggplot(File, aes(y=File[,input$dependent], x= File[,input$independent])) +
+    p <- ggplot(File(), aes(y=File[,input$dependent], x= File[,input$independent])) +
       geom_point(size = input$size, col = "blue") +
       ggtitle(input$title)+RemoveGrid+scale_x_continuous(name=input$independent)+scale_y_continuous(name=input$dependent)+theme(plot.title=element_text(size=14,face="bold"))
     
@@ -42,7 +42,7 @@ library(dplyr)
                  Competencia_Teamplayer+Competencia_PensamientoCritico+Competencia_Negocio+Competencia_InnovacionYmotivacion,data=File)
     Frame$Predict<-predict(Modelo,Frame)
     
-    perf <- ggplot(File, aes(x=Desempeno_Primerano))+ geom_density(adjust=2,alpha=0.2,fill="dodgerblue")+RemoveGrid+geom_vline(xintercept=Frame$Predic, size=1.5, color="red")+geom_text(aes(x=Frame$Predic, label="Predicción desempeño candidato", y=0.1), colour="black", angle=90, vjust = 1.2, text=element_text(size=11))+ scale_x_continuous(name="Desempeño colaboradores actuales")+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.line.y=element_blank())
+    perf <- ggplot(File(), aes(x=Desempeno_Primerano))+ geom_density(adjust=2,alpha=0.2,fill="dodgerblue")+RemoveGrid+geom_vline(xintercept=Frame$Predic, size=1.5, color="red")+geom_text(aes(x=Frame$Predic, label="Predicción desempeño candidato", y=0.1), colour="black", angle=90, vjust = 1.2, text=element_text(size=11))+ scale_x_continuous(name="Desempeño colaboradores actuales")+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.line.y=element_blank())
     
     perf
     
