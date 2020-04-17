c176f3f0f69b162f811d447700a70ea75f85bf55
aliriojsp <32307581+aliriojsp@users.noreply.github.com>
2020-04-17 14:11:57 -0300

    Update init.R

--- init.R
+++ init.R
@@ -1,2 +1,3 @@
 install.packages("ggplot2", dependencies = TRUE)
 install.packages("dplyr", dependencies = TRUE)
+File<-read.csv("https://raw.githubusercontent.com/aliriojsp/simulador_bootcamp/master/File.csv")
