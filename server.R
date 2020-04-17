9d8ac63bb6b1e31a4bec95710ed9504f4049db7d
aliriojsp <32307581+aliriojsp@users.noreply.github.com>
2020-04-17 11:35:59 -0300

    Update server.R

--- server.R
+++ server.R
@@ -4,8 +4,8 @@ library(dplyr)
 
 shinyServer(function(input, output, session) { 
   
-  File <- reactive({File<-read.csv("https://raw.githubusercontent.com/aliriojsp/simulador_bootcamp/master/File.csv?token=AHWPS7KHQYH6UAFFGZML5SC6TG34S")%>%select(GÃ©nero=Gender,EducaciÃ³n=EducationHighest,ExperienciaPrevia=WorkExperience, Personalidad_apertura="ACPersonalityO", Personalidad_responsabilidad="ACPersonalityC",
-  Personalidad_extroversiÃ³n="ACPersonalityE", Personalidad_amabilidad= "ACPersonalityA", Personalidad_neuroticismo="ACPersonalityN",Competencia_TÃ©cnica="ACRatingINTCOMPA", Competencia_Teamplayer="ACRatingINTCOMPB",Competencia_PensamientoCritico="ACRatingINTCOMPC",Competencia_Negocio="ACRatingINTCOMPD",Competencia_InnovacionYmotivacion="ACRatingINTCOMPE", DesempeÃ±o_PrimeraÃ±o="Year1performanceRating")
+  File <- reactive({File<-read.csv("https://github.com/aliriojsp/simulador_bootcamp/blob/master/File.csv")%>%select(Genero=Gender,Educacion=EducationHighest,ExperienciaPrevia=WorkExperience, Personalidad_apertura="ACPersonalityO", Personalidad_responsabilidad="ACPersonalityC",
+  Personalidad_extroversion="ACPersonalityE", Personalidad_amabilidad= "ACPersonalityA", Personalidad_neuroticismo="ACPersonalityN",Competencia_Tecnica="ACRatingINTCOMPA", Competencia_Teamplayer="ACRatingINTCOMPB",Competencia_PensamientoCritico="ACRatingINTCOMPC",Competencia_Negocio="ACRatingINTCOMPD",Competencia_InnovacionYmotivacion="ACRatingINTCOMPE", Desempeno_Primerano="Year1performanceRating")
   })
     
   output$plot <- renderPlot({
@@ -29,24 +29,24 @@ shinyServer(function(input, output, session) {
     # Subset the gapminder dataset by the chosen continents
     RemoveGrid<- theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))
     
-    Género<- c(input$Género)
-    Educación<-c(input$Educación)
+    Genero<- c(input$Genero)
+    Educacion<-c(input$Educacion)
     ExperienciaPrevia<- c(input$ExperienciaPrevia)
     Personalidad_apertura<-c(input$Personalidad_apertura)
     Personalidad_responsabilidad<-c(input$Personalidad_responsabilidad)
     Personalidad_extroversión<-c(input$Personalidad_extroversión)
     Personalidad_amabilidad<-c(input$Personalidad_amabilidad)
-    Competencia_Técnica<-c(input$Competencia_Técnica)
+    Competencia_Tecnica<-c(input$Competencia_Tecnica)
     Competencia_Teamplayer<-c(input$Competencia_Teamplayer)
     Competencia_PensamientoCritico<-c(input$Competencia_PensamientoCritico)
     Competencia_Negocio<-c(input$Competencia_Negocio)
     Competencia_InnovacionYmotivacion<-c(input$Competencia_InnovacionYmotivacion)
-    Frame<- data.frame(Género,Educación,ExperienciaPrevia,Personalidad_apertura,Personalidad_responsabilidad,Personalidad_extroversión,Personalidad_amabilidad,Competencia_Técnica,Competencia_Teamplayer,Competencia_PensamientoCritico,Competencia_Negocio,Competencia_InnovacionYmotivacion)
-    Modelo<-lm(File$Desempeño_Primeraño~as.numeric(Género)+as.numeric(Educación)+as.numeric(ExperienciaPrevia)+Personalidad_apertura+Personalidad_responsabilidad+Personalidad_extroversión+Personalidad_amabilidad+Competencia_Técnica+
+    Frame<- data.frame(Genero,Educacion,ExperienciaPrevia,Personalidad_apertura,Personalidad_responsabilidad,Personalidad_extroversion,Personalidad_amabilidad,Competencia_Tecnica,Competencia_Teamplayer,Competencia_PensamientoCritico,Competencia_Negocio,Competencia_InnovacionYmotivacion)
+    Modelo<-lm(File$Desempeno_Primerano~as.numeric(Genero)+as.numeric(Educacion)+as.numeric(ExperienciaPrevia)+Personalidad_apertura+Personalidad_responsabilidad+Personalidad_extroversion+Personalidad_amabilidad+Competencia_Tecnica+
                  Competencia_Teamplayer+Competencia_PensamientoCritico+Competencia_Negocio+Competencia_InnovacionYmotivacion,data=File)
     Frame$Predict<-predict(Modelo,Frame)
     
-    perf <- ggplot(File, aes(x=Desempeño_Primeraño))+ geom_density(adjust=2,alpha=0.2,fill="dodgerblue")+RemoveGrid+geom_vline(xintercept=Frame$Predic, size=1.5, color="red")+geom_text(aes(x=Frame$Predic, label="Predicción desempeño candidato", y=0.1), colour="black", angle=90, vjust = 1.2, text=element_text(size=11))+ scale_x_continuous(name="Desempeño colaboradores actuales")+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.line.y=element_blank())
+    perf <- ggplot(File, aes(x=Desempeno_Primerano))+ geom_density(adjust=2,alpha=0.2,fill="dodgerblue")+RemoveGrid+geom_vline(xintercept=Frame$Predic, size=1.5, color="red")+geom_text(aes(x=Frame$Predic, label="Predicción desempeño candidato", y=0.1), colour="black", angle=90, vjust = 1.2, text=element_text(size=11))+ scale_x_continuous(name="Desempeño colaboradores actuales")+theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank(),axis.line.y=element_blank())
     
     perf
     
