ce772c6aa15617ca0cb81cc25d5771c32e00c974
aliriojsp <32307581+aliriojsp@users.noreply.github.com>
2020-04-18 00:24:02 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,16 +2,10 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-fluidPage(fileInput("File", "Choose CSV File",
-                accept = c(
-                "text/csv",
-                "text/comma-separated-values,text/plain",
-                ".csv")
-                ),
-        tags$hr(),
-        checkboxInput("header", "Header", TRUE)
-        ),
-                 
+fluidPage(
+  
+  fileInput('File', 'Choose CSV file',
+      accept=c('text/csv', 'text/comma-separated-values,text/plain')),                
     sidebarLayout(
     sidebarPanel(
       "¡Hola! Bienvenido/a a esta herramienta para practicar la generación de insights y predicciones con datos de RRHH. El objetivo es simular algunas dinamicas claves durante la fase de analisis en un proyecto de people analytics.",
